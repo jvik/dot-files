beb5e277f24865c30c913babc54bf631ae8c6b14
Jorgen V <mail@jvik.no>
2025-11-03 14:57:05 +0100

    Lots of minor stuff

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -34,7 +34,7 @@ config.front_end = "WebGpu"
 config.webgpu_power_preference = "HighPerformance"
 
 -- color config --
-config.color_scheme = "Gruvbox Dark (Gogh)"
+config.color_scheme = "Gruvbox (Gogh)"
 --config.color_scheme = "Catppuccin Frappe"
 --config.color_scheme = "Catppuccin Latte"
 --config.color_scheme = "rose-pine-dawn"
